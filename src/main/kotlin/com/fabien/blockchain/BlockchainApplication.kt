210a6e3f5fe08e4bfec92c48a22b085a9a35ad7f
Fabien Veron <fabien.veron@gmail.com>
2017-10-08 07:24:05 +0200

    Use java KeyPair. Sign tansaction and verify signature before adding to blockchain

--- src/main/kotlin/com/fabien/blockchain/BlockchainApplication.kt
+++ src/main/kotlin/com/fabien/blockchain/BlockchainApplication.kt
@@ -2,6 +2,9 @@ package com.fabien.blockchain
 
 import com.google.common.hash.Funnel
 import com.google.common.hash.Hashing
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.Signature
 import java.time.LocalDateTime
 import java.util.concurrent.ThreadLocalRandom
 
@@ -14,6 +17,9 @@ import java.util.concurrent.ThreadLocalRandom
 1. What is the current bitcoin leading zeros complexity ?
 2. Is adding one zero of complexity really double up the time in bitcoin ? (difficulty -> avg hashes : 2 -> 7,3 -> 15, 4->32, 5->65)
 3. my laptop hash capacity: 127960366 hashes in 66 sec. 1_938_793 hash/sec ~ 1.9MHash/sec
+4. What is the timestamp protocol used in bitcoin ?
+5. Is transaction in blockchain only about user give to another user (owing/giving money)
+6. Is the minor/contributor put in the block ? Do we keep track of the minor in the hash (and is it being hashed?)
 */
 
 
@@ -22,15 +28,18 @@ object Blockchain {
 
     private val chain = mutableListOf<Block>()
 
-    val transactions = mutableListOf<Transaction>()
+    val transactions = mutableListOf<SignedTransaction>()
 
-    fun addTransaction(transaction: Transaction): Boolean {
+    fun addTransaction(transaction: SignedTransaction): Boolean {
+        if (!transaction.verifySignature())
+            return false
+
         return transactions.add(transaction)
     }
 
-    data class MiningResult(val guess:Long, val counter:Long)
+    data class MiningResult(val guess: Long, val counter: Long)
 
-    fun mineTransactions(contributor: ByteArray, counterStatus: Int = 0, displayStatus: ((counter: Long) -> Unit) = {}): MiningResult {
+    fun mineTransactions(contributor: PublicKey, counterStatus: Int = 0, displayStatus: ((counter: Long) -> Unit) = {}): MiningResult {
         var counter: Long = 0
         var guess: Long
         do {
@@ -45,25 +54,26 @@ object Blockchain {
         return MiningResult(guess, counter)
     }
 
-    fun hashBlock(contributor: ByteArray, proof: Long): ByteArray {
-        val transactionsFunnel = Funnel<List<Transaction>> { transactions, sink ->
+    fun hashBlock(contributor: PublicKey, proof: Long): ByteArray {
+        val transactionsFunnel = Funnel<List<SignedTransaction>> { transactions, sink ->
             for (t in transactions) {
                 sink
-                        ?.putBytes(t?.publicKeySender)
-                        ?.putBytes(t?.pubicKeyReceiver)
+                        ?.putBytes(t?.publicKeySender.encoded)
+                        ?.putBytes(t?.pubicKeyReceiver.encoded)
                         ?.putDouble(t?.amount!!)
+                        ?.putBytes(t?.signature)
             }
         }
         return Hashing.sha256().newHasher()
                 .putObject(transactions, transactionsFunnel)
-                .putBytes(contributor)
+                .putBytes(contributor.encoded)
                 .putLong(proof)
 //                .putBytes(chain[chain.lastIndex].previousHash) // TODO:Check if previous hash is part of hashing (actually the whole block)
                 .hash().asBytes()
     }
 
-    fun addBlock(index: Long, contributor: ByteArray, proof: Long): Boolean {
-        val hash = hashBlock(contributor,proof)
+    fun addBlock(index: Long, contributor: PublicKey, proof: Long): Boolean {
+        val hash = hashBlock(contributor, proof)
         if (leadingZeroBits(hash) < proofDifficulty) {
             return false
         }
@@ -97,29 +107,36 @@ object Blockchain {
     }
 }
 
-class Transaction(val publicKeySender: ByteArray, val pubicKeyReceiver: ByteArray, val amount: Double)
+open class Transaction(val publicKeySender: PublicKey, val pubicKeyReceiver: PublicKey, val amount: Double)
+
+fun signTransaction(transaction: Transaction, privateKey: PrivateKey): ByteArray {
+    val signature = Signature.getInstance("SHA256withRSA")
+    signature.initSign(privateKey)
+    signature.update(transaction as ByteArray)
+    return signature.sign()
+}
+
+class SignedTransaction(transaction: Transaction, val signature: ByteArray) : Transaction(transaction.publicKeySender, transaction.pubicKeyReceiver, transaction.amount) {
+    fun verifySignature(): Boolean {
+        val signatureInst = Signature.getInstance("SHA256withRSA")
+        signatureInst.initVerify(this.publicKeySender)
+        signatureInst.update(this as ByteArray)
+        return signatureInst.verify(this.signature)
+    }
+}
 
 class Block(val index: Long,
             val transactions: List<Transaction>,
-            val contributor: ByteArray, // TODO: check if not in bitcoin blockchain ?
+            val contributor: PublicKey, // TODO: check if not in bitcoin blockchain ?
             val proof: Long,
             val previousHash: ByteArray) {
 
     val timestamp: LocalDateTime = LocalDateTime.now()
 }
 
-class RSAKeys(val publicKey: ByteArray, private val privateKey: ByteArray)
-
-fun MutableList<Int>.swap(index1: Int, index2: Int) {
-    val tmp = this[index1] // 'this' corresponds to the list this[index1] = this[index2]
-    this[index2] = tmp
-}
-
-
 fun ofUnsigned(int: Int): Byte {
     return if (int > Byte.MAX_VALUE) {
         (Byte.MIN_VALUE + (int and 0b0111_1111)).toByte()
-    }
-    else
+    } else
         int.toByte()
 }
