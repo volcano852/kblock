c743d08f61efd11fb82f6b04895d180bd81ec876
Fabien Veron <fabien.veron@gmail.com>
2017-10-14 00:05:12 +0200

    complete /chain endpoint

--- src/main/kotlin/com/fabien/kblock/KBlockController.kt
+++ src/main/kotlin/com/fabien/kblock/KBlockController.kt
@@ -1,13 +1,24 @@
 package com.fabien.kblock
 
+import com.fabien.kblock.SignedTransactionDto.Companion.toSignedTransactionDto
 import org.springframework.web.bind.annotation.*
 import java.security.KeyFactory
 import java.security.spec.X509EncodedKeySpec
+import java.time.LocalDateTime
 import java.util.*
 
 data class SignedTransactionDto(val sender:String,val receiver: String, val amount:Double, val signature : String, val random : Long) {
 
-    fun toTransaction() : SignedTransaction {
+    companion object {
+        fun toSignedTransactionDto(signedTransaction: SignedTransaction): SignedTransactionDto {
+            val publicKeySenderBase64 = Base64.getEncoder().encodeToString(signedTransaction.publicKeyReceiver.encoded)
+            val publicKeyReceiverBase64 = Base64.getEncoder().encodeToString(signedTransaction.publicKeySender.encoded)
+            val signatureBase64 = Base64.getEncoder().encodeToString(signedTransaction.signature)
+            return SignedTransactionDto(publicKeySenderBase64,publicKeyReceiverBase64,signedTransaction.amount,signatureBase64,signedTransaction.random)
+        }
+    }
+
+    fun toSignedTransaction() : SignedTransaction {
         val senderKey = X509EncodedKeySpec(Base64.getDecoder().decode(sender))
         val receiverKey = X509EncodedKeySpec(Base64.getDecoder().decode(receiver))
         val signatureBytes = Base64.getDecoder().decode(signature)
@@ -27,15 +38,33 @@ class TransactionController{
 
     @PostMapping()
     fun addTransaction(@RequestBody signedTransactionDto: SignedTransactionDto) : Boolean {
-        return Blockchain.addTransaction(signedTransactionDto.toTransaction())
+        return Blockchain.addTransaction(signedTransactionDto.toSignedTransaction())
     }
 }
+
+class BlockDto(val index: Long,
+            val transactions: List<SignedTransactionDto>,
+            val contributor: String, // TODO: check if not in bitcoin blockchain ?
+            val proof: Long,
+            val previousHash: String,
+            val timestamp: LocalDateTime ) {
+
+    companion object {
+        fun toBlockDto(block: Block) : BlockDto {
+            val transactionsDto = block.transactions.map { toSignedTransactionDto(it) }
+            val contributorBase64 = Base64.getEncoder().encodeToString(block.contributor.encoded)
+            val previousHashBase64 = Base64.getEncoder().encodeToString(block.previousHash)
+            return BlockDto(block.index, transactionsDto, contributorBase64, block.proof, previousHashBase64, block.timestamp)
+        }
+    }
+}
+
 
 @RestController
 @RequestMapping("/chain")
 class ChainController {
     @GetMapping()
-    fun getBlockchain() : List<Block> {
-        return Blockchain.chain
+    fun getBlockchain() : List<BlockDto> {
+        return Blockchain.chain.map { BlockDto.toBlockDto(it) }
     }
 }
